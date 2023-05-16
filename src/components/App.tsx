36a58ac162a889142ae1ce9dd466d7f08faf940e
Dan Chambers <danlechambre@icloud.com>
2023-05-16 12:24:01 +0100

    Implement multiple-category filter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { mapValues, uniqWith } from 'lodash';
 import { useEffect, useState } from 'react';
 
 /*
@@ -51,19 +52,70 @@ function getDateString(date: string) {
   return dt.toLocaleDateString();
 }
 
+/*
+4. Categories on posts all have unique IDs - 
+in a real world scenario you'd expect categories appearing across multiple posts to have the same ID.
+I brought lodash in to do this for me as I didn't want to lose too much time on this issue
+*/
+function getCategories(posts: PostData[]) {
+  const categories = posts.map((post) => post.categories).flat();
+  return uniqWith(categories, (a, b) => a.name === b.name);
+}
+
 const App: React.FC = () => {
   // 3. Set up a simple state/effect combo for getting the posts
   const [posts, setPosts] = useState<PostData[] | []>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
   useEffect(() => {
-    getPosts().then((posts) => setPosts(posts));
+    getPosts().then((posts) => {
+      setCategories(getCategories(posts));
+      setPosts(posts);
+    });
   }, []);
+
+  /* 
+  5. I could have easily implemented a single category filter - but wanted to challenge myself to set up multi-select
+  Ordinarily - I would have brought in a headless component (e.g. Headless UI), but wanted to try for a mostly native implementation
+  using standard checkboxes.
+  */
+  const [filterCategories, setFilterCategories] = useState<Record<string, boolean>>({});
+  useEffect(() => {
+    const obj: Record<string, boolean> = {};
+    categories.forEach((category) => {
+      obj[category.name] = false;
+    });
+    setFilterCategories(obj);
+  }, [categories]);
+
+  const handleUpdateCategoryFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = e.target;
+
+    setFilterCategories((prevState) => ({
+      ...prevState,
+      [value]: checked,
+    }));
+  };
+
+  const handleClearSelection = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const resetObj = mapValues({ ...filterCategories }, () => false);
+    setFilterCategories(resetObj);
+  };
+
+  const filterFn = (post: PostData) => {
+    if (Object.values(filterCategories).includes(true)) {
+      return post.categories.some((c) => filterCategories[c.name]);
+    }
+
+    return true;
+  };
 
   return (
     <main>
       <h1>Awesome Posts</h1>
       <section>
         <ul>
-          {posts.map((post) => (
+          {posts.filter(filterFn).map((post) => (
             <li key={post.id}>
               <article>
                 <h2>{post.title}</h2>
@@ -77,8 +129,8 @@ const App: React.FC = () => {
                   <footer>
                     <h3>Categories</h3>
                     <ul>
-                      {post.categories.map((cat) => (
-                        <li>{cat.name}</li>
+                      {post.categories.map((cat, i) => (
+                        <li key={`category-${i}`}>{cat.name}</li>
                       ))}
                     </ul>
                   </footer>
@@ -87,6 +139,29 @@ const App: React.FC = () => {
             </li>
           ))}
         </ul>
+      </section>
+
+      <section>
+        <fieldset>
+          <legend>Filter by category&colon;</legend>
+          {categories.map((category) => (
+            <div key={category.id}>
+              <input
+                type="checkbox"
+                id={category.id}
+                name="categories"
+                value={category.name}
+                onChange={handleUpdateCategoryFilter}
+                checked={filterCategories[category.name] ?? false}
+              />
+              <label htmlFor={category.id}>{category.name}</label>
+            </div>
+          ))}
+        </fieldset>
+
+        <button type="button" onClick={handleClearSelection}>
+          Clear Selection
+        </button>
       </section>
     </main>
   );
